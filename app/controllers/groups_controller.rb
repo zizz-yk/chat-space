160b26679a1ae3315b8ffa998253d8f39db90698
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-10-18 23:54:02 +0900

    ルートパス変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,9 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
+    @groups = current_user.groups
   end
+
   def new
     @group = Group.new
     @group.users << current_user
