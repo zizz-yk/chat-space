f01336d4aa21ce77241cd88895ad0cf3c9eea829
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-10-23 13:18:30 +0900

    投稿機能のテスト

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.order(created_at: :DESC).includes(:user)
   end
 
   def create
