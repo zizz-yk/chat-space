6ee63e1ce782f092b57c2ff028810a35ee4551fd
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-10-26 12:29:07 +0900

    jsファイル作成、非同期通信実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message){
+
+    var ChatMessage = (message.content)? `${message.content}` : "";
+    var ChatImage = (message.image)? `<img src="${message.image}">` : "";
+
+    var html = `<div class="message">
+                  <div class="message-top">
+                    <p class="message-top__name">${message.user_name}</p>
+                    <p class="message-top__date">${message.date}</p>
+                  </div>
+                  <div class ="message-bottom">
+                    ${ChatMessage}
+                    ${ChatImage}
+                  </div>
+                </div>`;
+
+  return html;
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-body').append(html);
+      $('.input-box__text').val('');
+      $('.input-box__file').val('');
+      $('.form__send-btn').prop('disabled', false);
+      $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight},"first");
+    })
+    .fail(function() {
+      alert("通信に失敗しました");
+    });
+  });
+});
