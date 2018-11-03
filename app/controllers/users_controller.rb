83eadec8cf099a96af179d75564cf7366a528ea9
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-11-03 14:52:52 +0900

    Where文をまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%", current_user.id)
     respond_to do |format|
       format.html
       format.json
