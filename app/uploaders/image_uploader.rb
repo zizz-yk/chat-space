c5d38528599604eac8049d2e7147b33602e141e3
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-11-07 19:06:07 +0900

    画像サイズ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,11 +3,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [500, 500]
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
