5f85939e5218544db65688cf566024055be006ab
zizz-yk <miyauchi@MiyauchinoMacBook-ea.local>
2018-11-09 02:48:27 +0900

    Test15

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,80 +1,74 @@
-$(document).on('turbolinks:load', function() {
-   function buildHTML(message){
+$(function(){
+  function buildHTML(message){
 
-     var chatMessage = (message.content)? `${message.content}` : "";
-     var chatImage = (message.image)? `<img src="${message.image}">` : "";
+    var chatMessage = (message.content)? `${message.content}` : "";
+    var chatImage = (message.image)? `<img src="${message.image}">` : "";
 
-     var html = `<div class="message" data-message-id="${message.id}">
-                   <div class="message-top">
-                     <p class="message-top__name">${message.user_name}</p>
-                     <p class="message-top__date">${message.date}</p>
-                   </div>
-                   <div class ="message-bottom">
-                     ${chatMessage}<br/>
-                     ${chatImage}
-                   </div>
-                 </div>`;
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="message-top">
+                    <p class="message-top__name">${message.user_name}</p>
+                    <p class="message-top__date">${message.date}</p>
+                  </div>
+                  <div class ="message-bottom">
+                    ${chatMessage}<br/>
+                    ${chatImage}
+                  </div>
+                </div>`;
 
-   return html;
-   }
+  return html;
+  }
 
 
-   $('#new_message').on('submit', function(e){
-     e.preventDefault();
-     var formData = new FormData(this);
-     var url = $(this).attr('action');
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
 
-     $.ajax({
-       url: url,
-       type: "POST",
-       data: formData,
-       dataType: 'json',
-       processData: false,
-       contentType: false
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data){
-      if (data.length !== 0){
-        var html = buildHTML(data);
-        $('.chat-body').append(html);
-        $('#new_message')[0].reset();
-        $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight},"first");
-      }
+      var html = buildHTML(data);
+      $('.chat-body').append(html);
+      $('.input-box__text').val('');
+      $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight},"first");
     })
     .fail(function(){
       alert("通信に失敗しました");
-     })
-     .always(function(){
-       $('.form__send-btn').prop('disabled', false);
-     });
+    })
+    .always(function(){
+      $('.form__send-btn').prop('disabled', false);
+    });
 
-   });
-
-
-
-   var interval = setInterval(function(){
-   if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-
-   $.ajax({
-     type: 'GET',
-     url: location.href,
-     dataType: 'json'
-   })
-   .done(function(messages) {
-     var insertHTML = '';
-     messages.forEach(function(message) {
-       insertHTML += buildHTML(message);
-     });
-     $('.chat-body').prepend(insertHTML);
-   })
-   .fail(function(messages) {
-     alert('自動更新に失敗しました');
-   });
-   } else {
-     clearInterval(interval);
-    }} , 5000 );
-
- });
+  });
 
 
 
+  var interval = setInterval(function(){
+  if (window.location.href.match(/\/groups\/\d+\/messages/)) {
 
+  $.ajax({
+    type: 'GET',
+    url: location.href,
+    dataType: 'json'
+  })
+  .done(function(messages) {
+    var insertHTML = '';
+    messages.forEach(function(message) {
+      insertHTML += buildHTML(message);
+    });
+    $('.chat-body').prepend(insertHTML);
+  })
+  .fail(function(messages) {
+    alert('自動更新に失敗しました');
+  });
+  } else {
+    clearInterval(interval);
+   }} , 5000 );
+
+});
